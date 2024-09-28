8185ef771ee32f0270fa321f3642306975bf7eb2
Kristina Nikiforova <kristina.nik93@gmail.com>
2024-09-28 13:52:43 +0300

    deleted unused classes

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from .controllers import BlogController
+from .services.blog_service import BlogService
 
-blog_controller = BlogController()
+blog_service = BlogService()
 
 def post_list(request):
-    posts = blog_controller.get_all_posts()
+    posts = blog_service.get_all_posts()
     return render(request, 'post_list.html', {'posts': posts})
 
 def post_detail(request, post_id):
-    post, comments = blog_controller.get_post_details(post_id)
+    post = blog_service.get_post_by_id(post_id)
+    comments = blog_service.get_comments_for_post(post_id) if post else []
     return render(request, 'post_detail.html', {'post': post, 'comments': comments})
